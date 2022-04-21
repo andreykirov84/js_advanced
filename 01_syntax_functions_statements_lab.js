2cd6bd110c9c674aaea97a3c1d8d1d85c4fd6c06
Andrey Kirov, PhD <andrey_kirov@mail.bg>
2022-04-21 13:24:37 +0300

    add some solutions for Exercise: Syntax, Functions and Statements

--- 01_syntax_functions_statements_lab.js
+++ 01_syntax_functions_statements_lab.js
@@ -130,10 +130,6 @@ function getDaysInMonth(month, year) {
     console.log(new Date(year, month, 0).getDate());
 }
 
-getDaysInMonth(1, 2012)
-getDaysInMonth(2, 2021)
-
-
 // 09.	Square of Stars
 function squareOfStars(value = 5){
     let symbol = '* ';
